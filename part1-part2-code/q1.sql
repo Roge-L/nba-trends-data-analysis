b2ef25d6e73eaaea3defcbe16701ec80c2d71fca
Lam Roger <lamroger@teach.cs.toronto.edu>
2021-11-04 21:44:08 -0400

    Finish Untested Q1 and Q2

--- part1-part2-code/q1.sql
+++ part1-part2-code/q1.sql
@@ -14,11 +14,40 @@ CREATE TABLE q1(
 -- Do this for each of the views that define your intermediate steps.  
 -- (But give them better names!) The IF EXISTS avoids generating an error 
 -- the first time this file is imported.
-DROP VIEW IF EXISTS intermediate_step CASCADE;
-
+DROP VIEW IF EXISTS NoReviews CASCADE;
+DROP VIEW IF EXISTS Sales CASCADE;
+DROP VIEW IF EXISTS AtLeastThree CASCADE;
+DROP VIEW IF EXISTS EligibleItems CASCADE;
+DROP VIEW IF EXISTS Answer CASCADE;
 
 -- Define views for your intermediate steps here:
+CREATE VIEW NoReviews AS
+    SELECT IID
+    FROM Item
+    EXCEPT
+    SELECT DISTINCT IID
+    FROM Review;
 
+CREATE VIEW Sales AS
+    SELECT LineItem.PID, IID, quantity, Customer.CID, d, email, lastName, firstName
+    FROM LineItem 
+        JOIN Purchase ON LineItem.PID = Purchase.PID 
+        JOIN Customer ON Customer.CID = Purchase.CID;
+
+CREATE VIEW AtLeastThree AS
+    SELECT s1.IID
+    FROM Sales s1
+        JOIN Sales s2 ON s1.PID = s2.PID
+        JOIN Sales s3 ON s2.PID = s3.PID
+    WHERE s1.IID < s2.IID AND s2.IID < s3.IID;
+
+CREATE VIEW EligibleItems AS
+    SELECT AtLeastThree.IID
+    FROM NoReviews JOIN AtLeastThree ON NoReviews.IID = AtLeastThree.IID;
+
+CREATE VIEW Answer AS
+    SELECT CID, firstName, lastName, email
+    FROM Sales JOIN EligibleItems ON Sales.IID = EligibleItems.IID;
 
 -- Your query that answers the question goes below the "insert into" line:
-insert into q1
+-- insert into q1
--- part1-part2-code/q2.sql
+++ part1-part2-code/q2.sql
@@ -13,11 +13,56 @@ create table q2(
 -- Do this for each of the views that define your intermediate steps.  
 -- (But give them better names!) The IF EXISTS avoids generating an error 
 -- the first time this file is imported.
-DROP VIEW IF EXISTS intermediate_step CASCADE;
+DROP VIEW IF EXISTS Helpful CASCADE;
+DROP VIEW IF EXISTS TotalReviews CASCADE;
+DROP VIEW IF EXISTS Scores CASCADE;
+DROP VIEW IF EXISTS VeryHelpful CASCADE;
+DROP VIEW IF EXISTS SomewhatHelpful CASCADE;
+DROP VIEW IF EXISTS NotHelpful CASCADE;
+DROP VIEW IF EXISTS Answer CASCADE;
 
 
 -- Define views for your intermediate steps here:
+CREATE VIEW Helpful AS
+    SELECT reviewer, count(reviewer) AS num_helpful
+    FROM Helpfulness
+    WHERE helpfulness = 't'
+    GROUP BY reviewer;
 
+CREATE VIEW TotalReviews AS
+    SELECT reviewer, count(reviewer) AS num_reviews
+    FROM Helpfulness
+    GROUP BY reviewer;
+
+CREATE VIEW Scores AS
+    SELECT Helpful.reviewer, num_helpful, num_reviews, CAST(num_helpful AS DECIMAL) / num_reviews AS helpfulness_score
+    FROM Helpful JOIN TotalReviews ON Helpful.reviewer = TotalReviews.reviewer;
+
+CREATE VIEW VeryHelpful AS
+    SELECT *, 'Very Helpful' AS helpfulness_category
+    FROM Scores
+    WHERE helpfulness_score >= 0.8;
+
+CREATE VIEW SomewhatHelpful AS
+    SELECT *, 'Somewhat Helpful' AS helpfulness_category
+    FROM Scores
+    WHERE helpfulness_score < 0.8 AND helpfulness_score >= 0.5;
+
+CREATE VIEW NotHelpful AS
+    SELECT *, 'Not Helpful' AS helpfulness_category
+    FROM Scores
+    WHERE helpfulness_score < 0.5;
+
+CREATE VIEW Categorized AS
+    (SELECT * FROM VeryHelpful)
+    UNION
+    (SELECT * FROM SomewhatHelpful)
+    UNION
+    (SELECT * FROM NotHelpful);
+
+CREATE VIEW Answer AS
+    SELECT CID, firstName, helpfulness_category
+    FROM Categorized JOIN Customer ON Categorized.reviewer = Customer.CID;
 
 -- Your query that answers the question goes below the "insert into" line:
-insert into q2
+-- insert into q2
