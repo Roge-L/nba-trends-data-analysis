ae664d15ea458aee0fd30c58dd93fd5a2172a149
Lam Roger <lamroger@teach.cs.toronto.edu>
2021-11-09 00:59:16 -0500

    Make Progress in q3 and q4

--- part1-part2-code/q3.sql
+++ part1-part2-code/q3.sql
@@ -14,6 +14,7 @@ CREATE TABLE q3 (
 -- (But give them better names!) The IF EXISTS avoids generating an error 
 -- the first time this file is imported.
 DROP VIEW IF EXISTS Categories CASCADE;
+DROP VIEW IF EXISTS GoodCustomersDetailed CASCADE;
 DROP VIEW IF EXISTS GoodCustomers CASCADE;
 
 
@@ -22,13 +23,19 @@ CREATE VIEW Categories AS
     SELECT DISTINCT category
     FROM Item;
     
-CREATE VIEW GoodCustomers AS
-    SELECT Customer.CID, Purchase.PID, d, Review.rating, category, comment
+CREATE VIEW GoodCustomersDetailed AS
+    SELECT Customer.CID, Purchase.PID, Item.IID, d, Review.rating, category, comment
     FROM Customer
         JOIN Purchase ON Customer.CID = Purchase.CID
         JOIN Review ON Customer.CID = Review.CID
         JOIN Item ON Review.IID = Item.IID
     WHERE comment IS NOT NULL;
 
+CREATE VIEW GoodCustomers AS
+    SELECT DISTINCT CID
+    FROM GoodCustomersDetailed;
+
+
+    
 -- Your query that answers the question goes below the "insert into" line:
 -- insert into q3
--- part1-part2-code/q4.sql
+++ part1-part2-code/q4.sql
@@ -15,11 +15,52 @@ CREATE TABLE q4 (
 -- Do this for each of the views that define your intermediate steps.  
 -- (But give them better names!) The IF EXISTS avoids generating an error 
 -- the first time this file is imported.
-DROP VIEW IF EXISTS intermediate_step CASCADE;
-
+DROP VIEW IF EXISTS Purchases2020 CASCADE;
+DROP VIEW IF EXISTS MonthlySales2020 CASCADE;
 
 -- Define views for your intermediate steps here:
+CREATE VIEW Purchases2020 AS
+    SELECT Purchase.PID, Purchase.CID, Item.IID, Item.category, Purchase.d, EXTRACT(MONTH FROM Purchase.d) AS month, Item.price * LineItem.quantity AS saleprice
+    FROM Purchase
+        JOIN LineItem ON Purchase.PID = LineItem.PID
+        JOIN Item ON LineItem.IID = Item.IID;
+    -- WHERE EXTRACT(YEAR FROM d) = 2019;
+
+CREATE VIEW MonthlySales2020 AS
+    SELECT category, month, SUM(saleprice) AS sales
+    FROM Purchases2020
+    GROUP BY category, month;
+
+CREATE VIEW MaxPerMonth AS
+    SELECT month, MAX(sales) as maxSales
+    FROM MonthlySales2020
+    GROUP BY month;
+
+CREATE VIEW MinPerMonth AS
+    SELECT month, MIN(sales) as minSales
+    FROM MonthlySales2020
+    GROUP BY month;
+
+CREATE VIEW MaxCategories AS
+    SELECT category, MonthlySales2020.month, maxSales
+    FROM MonthlySales2020
+        JOIN MaxPerMonth ON MonthlySales2020.sales = MaxPerMonth.maxSales AND MonthlySales2020.month = MaxPerMonth.month;
+
+CREATE VIEW MinCategories AS
+    SELECT category, MonthlySales2020.month, minSales
+    FROM MonthlySales2020
+        JOIN MinPerMonth ON MonthlySales2020.sales = MinPerMonth.minSales AND MonthlySales2020.month = MinPerMonth.month;
+
+CREATE VIEW Answer AS
+    SELECT MonthlySales2020.month, 
+        MaxCategories.category AS highestCategory, 
+        MaxCategories.maxSales AS highestSalesValue, 
+        MinCategories.category AS lowestCategory, 
+        MinCategories.minSales AS lowestSalesValue
+    FROM MonthlySales2020
+        JOIN MaxCategories ON MonthlySales2020.month = MaxCategories.month
+        JOIN MinCategories ON MonthlySales2020.month = MinCategories.month;
 
 
 -- Your query that answers the question goes below the "insert into" line:
-insert into q4
+-- insert into q4
