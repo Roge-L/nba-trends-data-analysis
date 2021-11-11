ed2e7629a6be7cebd9fd472979bf5e513b796fe5
Lam Roger <lamroger@teach.cs.toronto.edu>
2021-11-11 14:59:08 -0500

    Finish Untested q7 and q8

--- part1-part2-code/q7.sql
+++ part1-part2-code/q7.sql
@@ -7,13 +7,21 @@ SET SEARCH_PATH TO Recommender;
 -- Do this for each of the views that define your intermediate steps.  
 -- (But give them better names!) The IF EXISTS avoids generating an error 
 -- the first time this file is imported.
-DROP VIEW IF EXISTS intermediate_step CASCADE;
-
+DROP VIEW IF EXISTS Last24Hours CASCADE;
+DROP VIEW IF EXISTS FishyPurchases CASCADE;
 
 -- Define views for your intermediate steps here:
+CREATE VIEW Last24Hours AS
+    SELECT *, DENSE_RANK() OVER (PARTITION BY CID ORDER BY d) AS purchaseOrder
+    FROM Purchase
+    WHERE d >= NOW() - '1 day'::INTERVAL;
 
+CREATE VIEW FishyPurchases AS
+    (SELECT * FROM Last24Hours)
+    EXCEPT
+    (SELECT * FROM Last24Hours WHERE purchaseOrder <= 5);
 
 -- Your SQL code that performs the necessary deletions goes here:
 
-
-
+DELETE FROM Purchase 
+WHERE PID IN (SELECT PID FROM FishyPurchases);
--- part1-part2-code/q8.sql
+++ part1-part2-code/q8.sql
@@ -7,10 +7,24 @@ SET SEARCH_PATH TO Recommender;
 -- Do this for each of the views that define your intermediate steps.  
 -- (But give them better names!) The IF EXISTS avoids generating an error 
 -- the first time this file is imported.
-DROP VIEW IF EXISTS intermediate_step CASCADE;
-
+DROP VIEW IF EXISTS HotItems CASCADE;
 
 -- Define views for your intermediate steps here:
-
+CREATE VIEW HotItems AS
+    SELECT IID, SUM(quantity)
+    FROM LineItem
+    GROUP BY IID HAVING SUM(quantity) >= 10;
 
 -- Your SQL code that performs the necessary updates goes here:
+
+UPDATE Item
+SET price = price - (price * .2)
+WHERE price >= 10 AND price <= 50;
+
+UPDATE Item
+SET price = price - (price * .3)
+WHERE price > 50 AND price < 100;
+
+UPDATE Item
+SET price = price - (price * .5)
+WHERE price >= 100;
